ada8496c1e60bf834ed015f05fd3d25f946b3c64
Ivan <exemple@gmail.com>
2022-07-15 23:42:23 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+// // alert('hello');
+
+// // const result = confirm('Are you here')
+// //  console.log(confirm);
+
+
+// // const answer = +promt ('вам есть 18?', "18")
+
+// // console.log(typeof(answer));
+
+
+// const answers = [];
+
+// // answers[0] = prompt('Как ваше имя?', '');
+// // answers[1] = prompt('Как ваше фамилия?', '');
+// // answers[2] = prompt('сколько вам лет?', '');
+
+// console.log(answers);
+
+
+console.log('arr' + "- object");
+console.log(4 +  +" 5");
+
+
+let incr = 10,
+    decr = 10;
+
+    // ++incr;
+    // --decr;
+
+    console.log(++incr);
+    console.log(--decr);
+
+   console.log(2 + 2 * 2 == '8');
+
+const isChecked = true,
+      isClose = false;
+
+      console.log(isChecked   isClose);
+
